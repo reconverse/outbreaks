16c037c1931ad847bc50c9e99057c7d76b906776
Thibaut Jombart <thibautjombart@gmail.com>
2016-06-21 19:13:14 +0100

    fixed tests

--- tests/testthat/test_RData.R
+++ tests/testthat/test_RData.R
@@ -2,16 +2,18 @@ context("RData objects distributed with the package")
 
 test_that("ebola.sim and ebola.sim.clean are OK", {
     ## dimensions
-    expect_identical(ncol(ebola.sim), ncol(ebola.sim.clean))
-    expect_equal(nrow(ebola.sim), 5889)
-    expect_equal(nrow(ebola.sim.clean), 5830)
+    expect_identical(ncol(ebola.sim$linelist), ncol(ebola.sim.clean$linelist))
+    expect_equal(nrow(ebola.sim$linelist), 5889)
+    expect_equal(nrow(ebola.sim.clean$linelist), 5689)
 
     ## names
     expect_identical(names(ebola.sim), names(ebola.sim.clean))
+    expect_identical(names(ebola.sim$linelist), names(ebola.sim.clean$linelist))
+    expect_identical(names(ebola.sim$contacts), names(ebola.sim.clean$contacts))
 
     ## check dates
-    id.dates <- grep("date", names(ebola.sim))
-    expect_true(all(sapply(ebola.sim[id.dates], class)=="Date"))
-    expect_true(all(sapply(ebola.sim.clean[id.dates], class)=="Date"))
+    id.dates <- grep("date", names(ebola.sim$linelist))
+    expect_true(all(sapply(ebola.sim$linelist[id.dates], class)=="Date"))
+    expect_true(all(sapply(ebola.sim.clean$linelist[id.dates], class)=="Date"))
 
 })
